ef017bed3d024f6d850fae4e12d6466b4aac1a02
robiul003 <97870032+robiul003@users.noreply.github.com>
2025-02-07 15:47:48 +0600

    Create ParticlesBackground.js

---
+++ components/ParticlesBackground.js
@@ -0,0 +1,65 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const ParticlesBackground = () => {
+    const particlesInit = async (main) => {
+        await loadFull(main);
+    };
+
+    return (
+        <Particles
+            id="tsparticles"
+            init={particlesInit}
+            options={{
+                background: {
+                    color: "#000"
+                },
+                particles: {
+                    number: {
+                        value: 100,
+                        density: {
+                            enable: true,
+                            area: 800
+                        }
+                    },
+                    color: {
+                        value: "#ffffff"
+                    },
+                    shape: {
+                        type: "circle"
+                    },
+                    opacity: {
+                        value: 0.5,
+                        random: true
+                    },
+                    size: {
+                        value: 3,
+                        random: true
+                    },
+                    move: {
+                        enable: true,
+                        speed: 2,
+                        direction: "none",
+                        random: false,
+                        straight: false,
+                        outModes: "out"
+                    }
+                },
+                interactivity: {
+                    events: {
+                        onHover: {
+                            enable: true,
+                            mode: "repulse"
+                        },
+                        onClick: {
+                            enable: true,
+                            mode: "push"
+                        }
+                    }
+                }
+            }}
+        />
+    );
+};
+
+export default ParticlesBackground;
